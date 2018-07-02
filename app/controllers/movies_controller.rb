56f79eadc04392416c122ddda24c276dbdd7d9d5
Robert Morris <obviouslyrob@gmail.com>
2018-07-02 20:49:31 +0000

    Adds Simple Form, connects functionality on new.html.erb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,8 @@ class MoviesController < ApplicationController
         @movies = Movie.all
         @movies = Movie.paginate(:page => params[:page], :per_page => 30)
     end
+    
+    def new
+        @movie = Movie.new
+    end
 end
